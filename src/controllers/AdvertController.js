1feaf1dde96df6a0d6f7672b0effb3ef42e805f3
Roman <efim1382@gmail.com>
2017-07-15 16:25:16 +0300

    delete comments

--- src/controllers/AdvertController.js
+++ src/controllers/AdvertController.js
@@ -6,36 +6,6 @@ exports.getAdverts = function(req, res) {
   Advert.find({}, function(err, adverts) {
     res.send(adverts);
   });
-  // Advert.find({}, function(err, adverts) {
-  //   let array = [];
-
-  //   let chain = new Promise(function(resolve, reject) {
-  //     adverts.forEach((elem, index) => {
-  //       User.findOne({ '_id': elem.userId }, function(err, user) {
-  //         array.push({
-  //           userId: elem.userId,
-  //           title: elem.title,
-  //           image: elem.image,
-  //           date: elem.date,
-  //           price: elem.price,
-  //           category: elem.category,
-  //           description: elem.description,
-  //           userImage: user.photo,
-  //           userName: user.name,
-  //           address: user.address,
-  //         });
-
-  //         if (index === adverts.length - 1) {
-  //           resolve(array);
-  //         }
-  //       });
-  //     });
-  //   });
-
-  //   chain.then(value => {
-  //     res.send(value);
-  //   });
-  // });
 };
 
 exports.addAdvert = function(req, res) {
