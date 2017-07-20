44ffa7b5c6d5c30a2b7dec79bb3f942df3beb3e9
Roman <efim1382@gmail.com>
2017-07-20 13:12:50 +0300

    fix upload files

--- src/api/adverts.js
+++ src/api/adverts.js
@@ -2,34 +2,9 @@ module.exports = function(app) {
   const urlPath = '/api/v1';
   const Advert = require('../controllers/AdvertController');
 
-  const fs = require('fs');
-
-  const multer  = require('multer');
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      if (!fs.existsSync('upload')) {
-        fs.mkdirSync('upload');
-      }
-
-      if (!fs.existsSync('upload/adverts')) {
-        fs.mkdirSync('upload/adverts');
-      }
-
-      if (!fs.existsSync(`upload/adverts/${req.body.userId}`)) {
-        fs.mkdirSync(`upload/adverts/${req.body.userId}`);
-      }
-
-      cb(null, `upload/adverts/${req.body.userId}`);
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-  })
-  const upload = multer({ storage: storage })
-
   app.route(`${urlPath}/adverts`).get(Advert.getAdverts);
   app.route(`${urlPath}/adverts/:id`).get(Advert.getOneAdvert);
   app.route(`${urlPath}/adverts/user/:id`).get(Advert.getUserAdverts);
-  app.post(`${urlPath}/adverts/add`, upload.single('image'), Advert.addAdvert);
-  app.post(`${urlPath}/adverts/:id/edit`, upload.single('image'), Advert.editAdvert);
+  app.post(`${urlPath}/adverts/add`, Advert.addAdvert);
+  app.post(`${urlPath}/adverts/:id/edit`, Advert.editAdvert);
 };
--- src/controllers/AdvertController.js
+++ src/controllers/AdvertController.js
@@ -2,6 +2,31 @@ const mongoose = require('mongoose');
 const Advert = mongoose.model('Advert');
 const User = mongoose.model('User');
 
+const fs = require('fs');
+
+const multer  = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    if (!fs.existsSync('upload')) {
+      fs.mkdirSync('upload');
+    }
+
+    if (!fs.existsSync('upload/adverts')) {
+      fs.mkdirSync('upload/adverts');
+    }
+
+    if (!fs.existsSync(`upload/adverts/${req.body.userId}`)) {
+      fs.mkdirSync(`upload/adverts/${req.body.userId}`);
+    }
+
+    cb(null, `upload/adverts/${req.body.userId}`);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage: storage }).single('image');
+
 exports.getAdverts = function(req, res) {
   Advert.find({}, function(err, adverts) {
     res.send(adverts);
@@ -9,36 +34,43 @@ exports.getAdverts = function(req, res) {
 };
 
 exports.addAdvert = function(req, res) {
-  let obj = {
-    userId: req.body.userId,
-    title: req.body.title,
-    date: req.body.date,
-    price: req.body.price,
-    category: req.body.category,
-    description: req.body.description,
-    image: `adverts/${req.body.userId}/${req.file.originalname}`,
-  }
-  let newAdvert = new Advert(obj);
+  upload(req, res, function() {
+    let obj = {
+      userId: req.body.userId,
+      title: req.body.title,
+      date: req.body.date,
+      price: req.body.price,
+      category: req.body.category,
+      description: req.body.description,
+      image: `adverts/${req.body.userId}/${req.file.originalname}`,
+    }
+    let newAdvert = new Advert(obj);
 
-  newAdvert.save(function(err, advert) {
-    res.send(advert);
+    newAdvert.save(function(err, advert) {
+      res.send(advert);
+    });
   });
 };
 
 exports.editAdvert = function(req, res) {
-  Advert.findOne({ _id: req.params.id }, function (err, advert) {
-    if (err) {
-      return;
-    }
+  upload(req, res, function() {
+    Advert.findOne({ _id: req.params.id }, function (err, advert) {
+      if (err) {
+        return;
+      }
 
-    advert.title = req.body.title,
-    advert.price = req.body.price,
-    advert.category = req.body.category,
-    advert.description = req.body.description,
-    advert.image = `adverts/${req.body.userId}/${req.file.originalname}`;
+      advert.title = req.body.title;
+      advert.price = req.body.price;
+      advert.category = req.body.category;
+      advert.description = req.body.description;
+      
+      if (req.file !== undefined) {
+        advert.image = `adverts/${req.body.userId}/${req.file.originalname}`;
+      }
 
-    advert.save(function(err, newAdvert) {
-      res.json(newAdvert);
+      advert.save(function(err, newAdvert) {
+        res.json(newAdvert);
+      });
     });
   });
 };
