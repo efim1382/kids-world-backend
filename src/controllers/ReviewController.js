c3fa2080efd1d3e9c5f345ee7fabd9f1bb80b219
Roman <efim1382@gmail.com>
2018-01-21 22:40:02 +0300

    fix reviews

--- src/controllers/ReviewController.js
+++ src/controllers/ReviewController.js
@@ -1,22 +1,99 @@
 const db = require('../database')();
 
+const { logger } = require('../functions');
+
+/**
+ * @api {post} /reviews/add addReview
+ * @apiGroup Review
+ *
+ * @apiDescription Добавление отзыва о пользователе
+ *
+ * @apiParam {Number} idAuthor Id автора отзыва.
+ * @apiParam {Number} idRecipient Id получателя.
+ * @apiParam {String} emotion Оценка пользователя (like, dislike).
+ * @apiParam {String} text Текст отзыва.
+ *
+ * @apiSuccessExample Success-Response:
+ *     {
+ *       "status": 200
+ *     }
+ */
 exports.addReview = function(req, res) {
   const { idAuthor, idRecipient, emotion, text } = req.body;
+
+  if (!idAuthor || !idRecipient || !emotion || !text) {
+    logger('Не заполнены все данные');
+
+    res.send({
+      status: 500,
+      message: 'Не заполнены все данные',
+    });
+
+    return;
+  }
 
   db.run(`
     INSERT INTO review (idAuthor, idRecipient, emotion, text)
     VALUES (?, ?, ?, ?)
   `, [idAuthor, idRecipient, emotion, text], function(error) {
     if (error) {
-      return console.error(error.message);
+      logger(error.message);
+
+      res.send({
+        status: 500,
+        message: 'Ошибка при добавлении отзыва',
+      });
+
+      return;
     }
 
-    res.status(200).send();
+    res.send({ status: 200 });
   });
 };
 
+/**
+ * @api {post} /reviews/user/:id getUserReviews
+ * @apiGroup Review
+ *
+ * @apiDescription Получить отзывы пользователя
+ *
+ * @apiParam {Number} id Id пользователя.
+ *
+ * @apiSuccessExample Success-Response:
+ *     {
+ *       "status": 200
+ *       "reviews": [{
+ *         "idRecipient": 1,
+ *         "idAuthor": 2,
+ *         "firstName": "Петр",
+ *         "lastName": "Петров",
+ *         "photo": "/images/user-image.jpg",
+ *         "text": "Хороший продавец",
+ *         "emotion": "like"
+ *       }, {
+ *         "idRecipient": 3,
+ *         "idAuthor": 5,
+ *         "firstName": "Иван",
+ *         "lastName": "Иванов",
+ *         "photo": "/images/user-image.jpg",
+ *         "text": "Плохой продавец",
+ *         "emotion": "dislike"
+ *       }]
+ *     }
+ */
 exports.getUserReviews = function(req, res) {
   const { id } = req.params;
+
+  if (!id) {
+    logger('Нет id');
+
+    res.send({
+      status: 500,
+      message: 'Ошибка при загрузке отзывов',
+    });
+
+    return;
+  }
 
   db.all(`
     SELECT user.id as idRecipient,
@@ -33,13 +110,28 @@ exports.getUserReviews = function(req, res) {
     DESC
   `, [id], function(error, reviews) {
     if (error) {
-      return console.error(error.message);
-    }
+      logger(error.message);
 
-    if (!reviews) {
+      res.send({
+        status: 500,
+        message: 'Ошибка при загрузке отзывов',
+      });
+
       return;
     }
 
-    res.status(200).send(reviews);
+    if (!reviews) {
+      res.send({
+        status: 200,
+        reviews: [],
+      });
+
+      return;
+    }
+
+    res.send({
+      status: 200,
+      reviews,
+    });
   });
 };
